6ba02552fc7c4b26a63146769c83fcf1038fdfd5
Ashah15 <ashaabdillahi53@gmail.com>
2020-05-19 22:28:12 +0300

    Modify comments_helper.rb, sessions_helper.rb && show.html.erb

--- twitter/app/helpers/comments_helper.rb
+++ twitter/app/helpers/comments_helper.rb
@@ -1,2 +1,10 @@
 module CommentsHelper
+	 def liked?(comment_id)
+    current_user.likes.exists?(comment_id: comment_id)
+  end
+
+  # Gets Like Id of a particular post liked by a current user
+  def current_user_liked(comment_id)
+    current_user.likes.find_by(comment_id: comment_id).id
+  end
 end
