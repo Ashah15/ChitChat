6ca6305d666dfe6f071aab329ae9e2f09ba19f83
Ashah15 <ashaabdillahi53@gmail.com>
2020-06-18 21:24:56 +0300

    correct linter code

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,9 @@ class User < ApplicationRecord
 
   # Returns the hash digest of the given string.
   def self.digest(string)
-     unless cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+     unless (cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+    BCrypt::Password.create(string, cost: cost))
      end
   end
 
